5fc29d5b897c7ec51466071d55f97625c5302836
KavinduAkash <kavinduakash520@gmail.com>
2026-04-05 06:18:46 +0530

    Update login system and dashboard

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from routes.login import login
-#from routes.dashboard import dashboard
+from routes.dashboard import dashboard
 #from routes.queue import queue
 #from routes.counter import counter
 #from routes.scanner import scanner
@@ -9,9 +9,17 @@ from routes.login import login
 app = Flask(__name__)
 app.secret_key = "your_secret_key_here"
 
+#  Disable browser caching (prevents back button access to dashboard)
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 # Register all routes
 app.register_blueprint(login)
-#app.register_blueprint(dashboard)
+app.register_blueprint(dashboard)
 #app.register_blueprint(queue)
 #app.register_blueprint(counter)
 #app.register_blueprint(scanner)
