38be2c0ea5eba2bfe9231b0293c2c7c32e9ae0a5
KavinduAkash <kavinduakash520@gmail.com>
2026-04-13 04:34:23 +0530

    Updated queue UI, dashboard, and added create_queue feature

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
-# 🔹 Import Blueprints from the routes folder
+# 🔹 Import Blueprints
 from routes.login import login
 from routes.dashboard import dashboard
 from routes.counterdashboard import counterdashboard
 from routes.createcounterstaff import createcounterstaff
 
+#  ADD THIS
+from routes.create_queue import createqueue
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key_here"
@@ -23,6 +25,9 @@ app.register_blueprint(login)
 app.register_blueprint(dashboard)
 app.register_blueprint(counterdashboard)
 app.register_blueprint(createcounterstaff)
+
+#  ADD THIS
+app.register_blueprint(createqueue)
 
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
